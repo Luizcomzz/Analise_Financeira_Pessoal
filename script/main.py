a90c22d3e7efbfaf59140b0e1f35377ec1b3091b
Luiz Honorio <luizedu.honorio@gmail.com>
2026-04-28 20:05:34 -0300

    alteração da tabela de dados

--- script/main.py
+++ script/main.py
@@ -1,5 +1,10 @@
-import panda as pd
-df = pd.read_csv("data/finanças 2025 - Vizualização.csv")
+import pandas as pd
 
-print(df.head())
-print(df.info)
+df = pd.read_csv("data/finanças_2026.csv", header=0, encoding= "utf-8")
+
+
+print(df.head()) #verificar os arquivos da base de dados
+print(df.info()) #verificar o tipo de dados de cada coluna 
+
+df.columns =  df.columns.str.lower().str.strip() #transformar todos os nomes em minusculos e exclui espaço em branco
+
