30a8455fb519d1d5d82b74b2dc6ec394e9dd3eb1
Luiz Honorio <luizedu.honorio@gmail.com>
2026-05-04 15:58:14 -0300

    criação tabela SQL e testes de consulta

--- script/main.py
+++ script/main.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import sqlite3 as sql
 
 df = pd.read_csv("data/finanças_2026.csv", header=0, encoding= "utf-8")
 
 
-print(df.head()) #verificar os arquivos da base de dados
-print(df.info()) #verificar o tipo de dados de cada coluna 
+#print(df.head()) #verificar os arquivos da base de dados
+#print(df.info()) #verificar o tipo de dados de cada coluna 
 
 df.columns = df.columns.str.lower().str.strip()
 df = df.rename(columns={
@@ -27,7 +28,7 @@ df['data'] = pd.to_datetime(df['data'])
 def categorizar(descricao):
     descricao = descricao.lower()
     
-    if "uber" in descricao or "99" in descricao or "gasolina" in descricao or "99" in descricao or "estacionamento" in descricao or "top" in descricao:
+    if "uber" in descricao or "99" in descricao or "gasolina" in descricao or "estacionamento" in descricao or "top" in descricao:
         return "Transporte"
     elif "ifood" in descricao or "restaurante" in descricao or "lanche" in descricao or "almoço" in descricao:
         return "Alimentação"
@@ -43,18 +44,37 @@ def categorizar(descricao):
         return "Outros"
 
 df['categoria'] = df['descricao'].apply(categorizar) #definir a variável categoria
-df['mes'] = df['data'].dt.to_period('M')
+df['mes'] = df['data'].dt.to_period('M').astype(str)
 
-
-print(df.head())
-print(df.info())
-
-#verificar os gastos totais 
-print("total Gastos: ", df['valor'].sum())
+#verificar os gastos totais usando python
+#print("total Gastos: ", df['valor'].sum())
       
-# verifica os gatos por categoria de maneira descendente
-print(df.groupby('categoria')['valor'].sum().sort_values(ascending=False))
+# verifica os gatos por categoria de maneira descendente usando python
+#print(df.groupby('categoria')['valor'].sum().sort_values(ascending=False))
 
 #verificar compras por mês
 print(df.groupby('mes')['valor'].sum())
+print()
 print(df.sort_values(by='valor', ascending=False).head(10))
+print()
+
+#criar uma tabela utilizando sql
+conn = sql.connect("financas.db")
+
+df.to_sql("gastos", conn, if_exists="replace", index=False)
+
+consulta = """
+SELECT
+    categoria,
+    SUM(valor) AS total
+FROM gastos
+GROUP BY categoria
+ORDER BY total DESC
+"""
+
+resultado = pd.read_sql_query(consulta, conn)
+
+print()
+print(resultado)
+
+conn.close()
