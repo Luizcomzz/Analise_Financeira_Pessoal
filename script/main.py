d1100f7f4f7bd3c4519be268067606224aebbe07
Luiz Honorio <luizedu.honorio@gmail.com>
2026-05-06 15:20:55 -0300

    acrescentar consultas SQL e produzir planilha tratada

--- script/main.py
+++ script/main.py
@@ -1,29 +1,33 @@
 import pandas as pd
 import sqlite3 as sql
 
-df = pd.read_csv("data/finanças_2026.csv", header=0, encoding= "utf-8")
+def carregar_dados(caminho):
+    return pd.read_csv(caminho, header=0, encoding= "utf-8")
 
 
-#print(df.head()) #verificar os arquivos da base de dados
-#print(df.info()) #verificar o tipo de dados de cada coluna 
-
-df.columns = df.columns.str.lower().str.strip()
-df = df.rename(columns={
+def tratar_dados(df):
+    df.columns = df.columns.str.lower().str.strip()
+    df = df.rename(columns={
     'descrição': 'descricao',
     'ano_mes': 'data'
-})
+    })
 
 
-df['valor'] = (
+    df['valor'] = (
     df['valor']
     .str.replace('R$', '', regex=False) # remove cifrão
     .str.replace('.', '', regex=False)   # remove milhar
     .str.replace(',', '.', regex=False)  # troca decimal
     .str.strip()
     .astype(float)
-)
-# converter data para modelo aaaa-mm-dd
-df['data'] = pd.to_datetime(df['data']) 
+    )
+    
+    # converter data para modelo aaaa-mm-dd
+    df['data'] = pd.to_datetime(df['data']) 
+    df['categoria'] = df['descricao'].apply(categorizar) #definir a variável categoria
+    df['mes'] = df['data'].dt.to_period('M').astype(str)
+
+    return df
 
 def categorizar(descricao):
     descricao = descricao.lower()
@@ -32,7 +36,7 @@ def categorizar(descricao):
         return "Transporte"
     elif "ifood" in descricao or "restaurante" in descricao or "lanche" in descricao or "almoço" in descricao:
         return "Alimentação"
-    elif "farmacia" in descricao or "consulta" in descricao:
+    elif "farmacia" in descricao or "consulta" in descricao or "dentista" in descricao:
         return "Saúde"
     elif "livro" in descricao or "curso" in descricao:
         return "Educação"
@@ -43,38 +47,87 @@ def categorizar(descricao):
     else:
         return "Outros"
 
-df['categoria'] = df['descricao'].apply(categorizar) #definir a variável categoria
-df['mes'] = df['data'].dt.to_period('M').astype(str)
-
-#verificar os gastos totais usando python
-#print("total Gastos: ", df['valor'].sum())
-      
-# verifica os gatos por categoria de maneira descendente usando python
-#print(df.groupby('categoria')['valor'].sum().sort_values(ascending=False))
-
-#verificar compras por mês
-print(df.groupby('mes')['valor'].sum())
-print()
-print(df.sort_values(by='valor', ascending=False).head(10))
-print()
-
 #criar uma tabela utilizando sql
-conn = sql.connect("financas.db")
+def criar_tabela(df, nome_banco):
+    conn = sql.connect(nome_banco)
 
-df.to_sql("gastos", conn, if_exists="replace", index=False)
+    df.to_sql("gastos", conn, if_exists="replace", index=False)
 
-consulta = """
-SELECT
-    categoria,
-    SUM(valor) AS total
-FROM gastos
-GROUP BY categoria
-ORDER BY total DESC
-"""
+    return conn
 
-resultado = pd.read_sql_query(consulta, conn)
+# Aqui pode consultar da mesma forma que faz com SQL
+def consultar_total(conn): #consulta do valor total gasto nessa tabela 
+    consulta = """
+    SELECT
+    SUM(valor) AS gastos
+    FROM gastos
+    """
+    return pd.read_sql_query(consulta, conn)
 
-print()
-print(resultado)
+def consultar_categorias(conn): #consulta de gastos por categorias
+    consulta = """
+    SELECT
+        categoria,
+        SUM(valor) AS total
+    FROM gastos
+    GROUP BY categoria
+    ORDER BY total DESC
+    """
+    return pd.read_sql_query(consulta, conn)
 
-conn.close()
+def consultar_ticket_medio(conn): #consultar gasto médio por mês
+    consulta = """
+    SELECT 
+        mes,
+        AVG(valor) AS media
+    FROM gastos
+    GROUP BY mes
+    ORDER BY mes DESC 
+    """
+    return pd.read_sql_query(consulta, conn)
+
+def consultar_evolucao(conn): #consultar evolução dos gastos em relação aos meses 
+    consulta = """
+    SELECT
+        mes,
+        SUM(valor) AS total
+    FROM gastos
+    GROUP BY mes
+    ORDER BY mes
+    """
+    return pd.read_sql_query(consulta, conn)
+
+def consultar_percentual(conn): #consulta de percentual gastos por categoria 
+    consulta = """
+    SELECT
+        categoria,
+        SUM(valor) AS total,
+        ROUND( SUM(valor) * 100.0 / (SELECT SUM(valor) FROM gastos), 2) AS percentual
+    FROM gastos
+    GROUP BY categoria
+    ORDER BY total DESC
+    """
+    return pd.read_sql_query(consulta, conn)
+
+def exportar_dados(df): #exportar dados tratados
+    df.to_csv("data/dados_tratados.csv", index=False)
+
+def main():
+    df = carregar_dados("data/finanças_2026.csv")
+
+    df = tratar_dados(df)
+
+    conn = criar_tabela(df, "financas.db")
+
+    print(consultar_total(conn), end= "\n\n")
+    print(consultar_categorias(conn), end= "\n\n")
+    print(consultar_evolucao(conn), end= "\n\n")
+    print(consultar_ticket_medio(conn), end= "\n\n")
+    print(consultar_percentual(conn), end= "\n\n")
+
+    exportar_dados(df)
+
+    conn.close()
+
+if __name__ == "__main__":
+    main()
